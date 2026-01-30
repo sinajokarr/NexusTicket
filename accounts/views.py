118b37b5b882b8cdd3c8955c24cbf8bc26863ed9
Sina Jokar <cnajokar11@yahoo.com>
2026-01-30 14:40:53 +0300

    update : app events

--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,7 @@
 from rest_framework import generics
-from .models import User
 from rest_framework.permissions import AllowAny
-from rest_framework.serializers import ModelSerializer
-from .serializers import UserSerializer  
-
-class UserSerializer(ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('email', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
-        return user
+from .models import User
+from .serializers import UserSerializer   
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
